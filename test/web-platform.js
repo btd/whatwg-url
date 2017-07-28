ced09f5cda80ac0993cfa776c7e2a99db90a744f
Sebastian Mayr <github@smayr.name>
2017-07-28 03:03:46 +0200

    Fix the DOMException shim to appear a bit more real

--- test/web-platform.js
+++ test/web-platform.js
@@ -11,40 +11,65 @@ const setterTestData = require("./web-platform-tests/setters_tests.json");
 
 const wptDir = path.join(__dirname, "web-platform-tests");
 
+const exceptionProps = {
+  INDEX_SIZE_ERR: 1,
+  DOMSTRING_SIZE_ERR: 2,
+  HIERARCHY_REQUEST_ERR: 3,
+  WRONG_DOCUMENT_ERR: 4,
+  INVALID_CHARACTER_ERR: 5,
+  NO_DATA_ALLOWED_ERR: 6,
+  NO_MODIFICATION_ALLOWED_ERR: 7,
+  NOT_FOUND_ERR: 8,
+  NOT_SUPPORTED_ERR: 9,
+  INUSE_ATTRIBUTE_ERR: 10,
+  INVALID_STATE_ERR: 11,
+  SYNTAX_ERR: 12,
+  INVALID_MODIFICATION_ERR: 13,
+  NAMESPACE_ERR: 14,
+  INVALID_ACCESS_ERR: 15,
+  VALIDATION_ERR: 16,
+  TYPE_MISMATCH_ERR: 17,
+  SECURITY_ERR: 18,
+  NETWORK_ERR: 19,
+  ABORT_ERR: 20,
+  URL_MISMATCH_ERR: 21,
+  QUOTA_EXCEEDED_ERR: 22,
+  TIMEOUT_ERR: 23,
+  INVALID_NODE_TYPE_ERR: 24,
+  DATA_CLONE_ERR: 25
+};
+
+class DOMException extends Error {
+  get name() {
+    if (!(this instanceof DOMException)) {
+      throw new TypeError("Wrong branding");
+    }
+    return super.name;
+  }
+}
+
+Object.defineProperty(DOMException.prototype, "name", {
+  get() {
+    if (!(this instanceof DOMException)) {
+      throw new TypeError("Wrong branding");
+    }
+    return super.name;
+  },
+  enumerable: true,
+  configurable: true
+});
+
+for (const prop in exceptionProps) {
+  DOMException[prop] = exceptionProps[prop];
+  DOMException.prototype[prop] = exceptionProps[prop];
+}
+
 function createSandbox() {
   const sandbox = {
     URL,
     URLSearchParams,
     // Shim for urlsearchparams-constructor.js
-    DOMException: {
-      prototype: {
-        INDEX_SIZE_ERR: 1,
-        DOMSTRING_SIZE_ERR: 2,
-        HIERARCHY_REQUEST_ERR: 3,
-        WRONG_DOCUMENT_ERR: 4,
-        INVALID_CHARACTER_ERR: 5,
-        NO_DATA_ALLOWED_ERR: 6,
-        NO_MODIFICATION_ALLOWED_ERR: 7,
-        NOT_FOUND_ERR: 8,
-        NOT_SUPPORTED_ERR: 9,
-        INUSE_ATTRIBUTE_ERR: 10,
-        INVALID_STATE_ERR: 11,
-        SYNTAX_ERR: 12,
-        INVALID_MODIFICATION_ERR: 13,
-        NAMESPACE_ERR: 14,
-        INVALID_ACCESS_ERR: 15,
-        VALIDATION_ERR: 16,
-        TYPE_MISMATCH_ERR: 17,
-        SECURITY_ERR: 18,
-        NETWORK_ERR: 19,
-        ABORT_ERR: 20,
-        URL_MISMATCH_ERR: 21,
-        QUOTA_EXCEEDED_ERR: 22,
-        TIMEOUT_ERR: 23,
-        INVALID_NODE_TYPE_ERR: 24,
-        DATA_CLONE_ERR: 25
-      }
-    }
+    DOMException
   };
   Object.assign(sandbox, testharness);
   vm.createContext(sandbox);
